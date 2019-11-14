4d1ab28df17140548a6c21f4f17d32abc370d80d
Ryan McKinley <ryantxu@gmail.com>
2019-11-14 15:46:27 -0800

    add slightly more complex example

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -6,7 +6,45 @@ interface Props extends PanelProps<SimpleOptions> {}
 
 export class SimplePanel extends PureComponent<Props> {
   render() {
-    const { options } = this.props;
-    return <div>Hello: {options.text}</div>;
+    const { options, data, width, height } = this.props;
+
+    return (
+      <div
+        style={{
+          position: 'relative',
+          width,
+          height,
+        }}
+      >
+        <svg
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+          }}
+          width={width}
+          height={height}
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
+        >
+          <g>
+            <circle style={{ fill: '#32a852' }} r={100} />
+          </g>
+        </svg>
+
+        <div
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            padding: '10px',
+          }}
+        >
+          <div>Count: {data.series.length}</div>
+          <div>{options.text}</div>
+        </div>
+      </div>
+    );
   }
 }
